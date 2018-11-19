6ee2b10d718152eb57608f1342d7d750f9a81d12
Joel Felipe Ferreira Gomes <joelfelipe07@gmail.com>
2018-11-19 10:31:49 -0300

    Start symbol table, with errors

--- include/symbol-table/SymbolTable.h
+++ include/symbol-table/SymbolTable.h
@@ -1,9 +1,10 @@
 #ifndef __SYMBOLTABLE__
 #define __SYMBOLTABLE__
 
-#include <unordered_multimap>
+#include <unordered_map>
 #include "ast/Node.h"
 #include <string>
+#include <utility>
 
 /**
  * Represents the symbol table.
@@ -11,48 +12,84 @@
  * */
 
 class StaticInfo {
-}
+
+   public: 
+        typedef std::pair<std::string, int> Type;
+};
+
+class MethodStaticInfo : public StaticInfo {
+    
+    public:
+       
+        // bool: True if Val
+        typedef std::tuple<std::string, Type, bool> FormalParam;
+        
+        Type retType;
+        std::vector<FormalParam> formalParams;
+        std::string codeLabel;
+};
+
+class VarStaticInfo : public StaticInfo {
+    
+    public:
+
+        Type varType;
+        
+};
 
 class Symbol {
 
     private:
 
         std::string name;
-        Symbol (std::string n) {
-            this.name = n
-        }
         static std::unordered_multimap<std::string, Symbol> dict;
 
     public:
 
-        std::string to_string() {
-            return this.name;
-        }
-        static Symbol symbol(std::string n){
-            Symbol s = dict.get(n)
-            if (s = nullptr){
-                s = new Symbol(u);
-                dict.put(u,s);
-            }
-            return s;
+        Symbol (std::string n) {
+            this->name = n;
         }
 
-}
+        std::string to_string() {
+            return this->name;
+        }
+        static Symbol symbol(std::string n) {
+            auto s = dict.find(n);
+            if (s == dict.end()) {
+                Symbol aux {n};
+                Symbol::dict.insert(std::make_pair<std::string, Symbol>(n,aux));
+                return aux;
+            }
+            return s->second;
+        }
+
+};
 
 class Table {
 
     protected:
 
-        std::unordered_multimap<Symbol, StaticInfo> table;
+        std::unordered_multimap<Symbol, std::shared_ptr<StaticInfo>> table;
     
     public:
 
-        Table();
-        void put(Symbol key, StaticInfo value);
-        Object get(Symbol key);
-        void beginScope();
-        void endScope();
+        Table(){}
+        void put(Symbol key, std::shared_ptr<StaticInfo> value){
+            table.insert(std::make_pair<Symbol, std::shared_ptr<StaticInfo>>(key, value));
+        }
+        StaticInfo get(Symbol key){
+            auto value = table.find(key);
+            if (value != table.end()){
+                value->second;
+            }
+        }
+        void beginScope(){
+            
+        }
+        void endScope(){
+            
+        }
         
-}
+};
 
 #endif
